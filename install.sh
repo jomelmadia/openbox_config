1136733d8307564670b8e493e5b9ebd6990326e3
jomel.madia <jomel.madia@gmail.com>
2019-11-01 00:49:30 +0800

    install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp autostart ~/.config/openbox
+echo "Autostart Copied"
+cp menu.xml ~/.config/openbox
+echo "Menu Copied"
+cp rc.xml ~/.config/openbox
+echo "Keyboard Shortcuts Copied"
