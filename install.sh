42b10140448d71dca2a5fd2667aa0d1876f21a2a
jomel.madia <jomel.madia@gmail.com>
2019-11-04 09:44:09 +0800

    add folder conditional

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-cp autostart ~/.config/openbox
-echo "Autostart Copied"
-cp menu.xml ~/.config/openbox
-echo "Menu Copied"
-cp rc.xml ~/.config/openbox
-echo "Keyboard Shortcuts Copied"
+
+function copy_config() {
+  cp autostart ~/.config/openbox
+  echo "Autostart Copied"
+  cp menu.xml ~/.config/openbox
+  echo "Menu Copied"
+  cp rc.xml ~/.config/openbox
+  echo "Keyboard Shortcuts Copied"
+}
+
+if [[ -d /home/$USER/.config/openbox ]]; 
+then
+  copy_config
+else
+  mkdir /home/$USER/.config/openbox
+  copy_config
+fi
